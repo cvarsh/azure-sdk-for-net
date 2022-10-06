3a93140d3b77c3bf5c84f0d32c56e37d481f18d1
SDKAuto <sdkautomation@microsoft.com>
2022-10-06 12:36:26 +0000

    CodeGen from PR 21020 in Azure/azure-rest-api-specs Merge 7a32cb783af4b07f09fa007f52a7eb7eb00375be into 2ab94c062e06ab7d274e4fa7d603515d4e656cc7

---
+++ sdk/appservice/Azure.ResourceManager.Appservice/tests/appserviceManagementTestEnvironment.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.ResourceManager.Appservice.Tests
+{
+    public class appserviceManagementTestEnvironment : TestEnvironment
+    {
+    }
+}
