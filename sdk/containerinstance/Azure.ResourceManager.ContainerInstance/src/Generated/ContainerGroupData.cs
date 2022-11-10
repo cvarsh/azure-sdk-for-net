9f09cbc72c4e48a99b7261e2a5832d2a58428034
SDKAuto <sdkautomation@microsoft.com>
2022-11-10 18:36:37 +0000

    CodeGen from PR 21499 in Azure/azure-rest-api-specs Merge 64484dc8760571a2de7f5cfbc96861e4a0985a54 into 700edefeae3fa585ef5ff6fc509a3b916784d6a1

--- sdk/containerinstance/Azure.ResourceManager.ContainerInstance/src/Generated/ContainerGroupData.cs
+++ sdk/containerinstance/Azure.ResourceManager.ContainerInstance/src/Generated/ContainerGroupData.cs
@@ -26,13 +26,14 @@ namespace Azure.ResourceManager.ContainerInstance
         {
             Argument.AssertNotNull(containers, nameof(containers));
 
+            Zones = new ChangeTrackingList<string>();
             Containers = containers.ToList();
             ImageRegistryCredentials = new ChangeTrackingList<ContainerGroupImageRegistryCredential>();
             OSType = osType;
             Volumes = new ChangeTrackingList<ContainerVolume>();
             SubnetIds = new ChangeTrackingList<ContainerGroupSubnetId>();
             InitContainers = new ChangeTrackingList<InitContainerDefinitionContent>();
-            Zones = new ChangeTrackingList<string>();
+            Extensions = new ChangeTrackingList<DeploymentExtensionSpec>();
         }
 
         /// <summary> Initializes a new instance of ContainerGroupData. </summary>
@@ -42,6 +43,7 @@ namespace Azure.ResourceManager.ContainerInstance
         /// <param name="systemData"> The systemData. </param>
         /// <param name="tags"> The tags. </param>
         /// <param name="location"> The location. </param>
+        /// <param name="zones"> The zones for the container group. </param>
         /// <param name="identity"> The identity of the container group, if configured. </param>
         /// <param name="provisioningState"> The provisioning state of the container group. This only appears in the response. </param>
         /// <param name="containers"> The containers within the container group. </param>
@@ -63,9 +65,10 @@ namespace Azure.ResourceManager.ContainerInstance
         /// <param name="sku"> The SKU for a container group. </param>
         /// <param name="encryptionProperties"> The encryption properties for a container group. </param>
         /// <param name="initContainers"> The init containers for a container group. </param>
-        /// <param name="zones"> The zones for the container group. </param>
-        internal ContainerGroupData(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, IDictionary<string, string> tags, AzureLocation location, ManagedServiceIdentity identity, string provisioningState, IList<ContainerInstanceContainer> containers, IList<ContainerGroupImageRegistryCredential> imageRegistryCredentials, ContainerGroupRestartPolicy? restartPolicy, ContainerGroupIPAddress ipAddress, ContainerInstanceOperatingSystemType osType, IList<ContainerVolume> volumes, ContainerGroupInstanceView instanceView, ContainerGroupDiagnostics diagnostics, IList<ContainerGroupSubnetId> subnetIds, ContainerGroupDnsConfiguration dnsConfig, ContainerGroupSku? sku, ContainerGroupEncryptionProperties encryptionProperties, IList<InitContainerDefinitionContent> initContainers, IList<string> zones) : base(id, name, resourceType, systemData, tags, location)
+        /// <param name="extensions"> extensions used by virtual kubelet. </param>
+        internal ContainerGroupData(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, IDictionary<string, string> tags, AzureLocation location, IList<string> zones, ManagedServiceIdentity identity, string provisioningState, IList<ContainerInstanceContainer> containers, IList<ContainerGroupImageRegistryCredential> imageRegistryCredentials, ContainerGroupRestartPolicy? restartPolicy, ContainerGroupIPAddress ipAddress, ContainerInstanceOperatingSystemType osType, IList<ContainerVolume> volumes, ContainerGroupInstanceView instanceView, ContainerGroupDiagnostics diagnostics, IList<ContainerGroupSubnetId> subnetIds, ContainerGroupDnsConfiguration dnsConfig, ContainerGroupSku? sku, ContainerGroupEncryptionProperties encryptionProperties, IList<InitContainerDefinitionContent> initContainers, IList<DeploymentExtensionSpec> extensions) : base(id, name, resourceType, systemData, tags, location)
         {
+            Zones = zones;
             Identity = identity;
             ProvisioningState = provisioningState;
             Containers = containers;
@@ -81,9 +84,11 @@ namespace Azure.ResourceManager.ContainerInstance
             Sku = sku;
             EncryptionProperties = encryptionProperties;
             InitContainers = initContainers;
-            Zones = zones;
+            Extensions = extensions;
         }
 
+        /// <summary> The zones for the container group. </summary>
+        public IList<string> Zones { get; }
         /// <summary> The identity of the container group, if configured. </summary>
         public ManagedServiceIdentity Identity { get; set; }
         /// <summary> The provisioning state of the container group. This only appears in the response. </summary>
@@ -132,7 +137,7 @@ namespace Azure.ResourceManager.ContainerInstance
         public ContainerGroupEncryptionProperties EncryptionProperties { get; set; }
         /// <summary> The init containers for a container group. </summary>
         public IList<InitContainerDefinitionContent> InitContainers { get; }
-        /// <summary> The zones for the container group. </summary>
-        public IList<string> Zones { get; }
+        /// <summary> extensions used by virtual kubelet. </summary>
+        public IList<DeploymentExtensionSpec> Extensions { get; }
     }
 }
