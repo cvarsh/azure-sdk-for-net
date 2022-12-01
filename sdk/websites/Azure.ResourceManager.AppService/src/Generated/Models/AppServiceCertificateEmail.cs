8cf214fbb3402213c87bdd7b6f09048fca0f46a6
SDKAuto <sdkautomation@microsoft.com>
2022-12-01 14:37:57 +0000

    CodeGen from PR 21724 in Azure/azure-rest-api-specs Merge 74bc737fb802d463e41f7ceba06fb71a1bae82a1 into 9842de8f8f11c09511fc00e903eb947f76cba0e6

--- sdk/websites/Azure.ResourceManager.AppService/src/Generated/Models/AppServiceCertificateEmail.cs
+++ sdk/websites/Azure.ResourceManager.AppService/src/Generated/Models/AppServiceCertificateEmail.cs
@@ -6,39 +6,29 @@
 #nullable disable
 
 using System;
-using Azure.Core;
-using Azure.ResourceManager.Models;
 
 namespace Azure.ResourceManager.AppService.Models
 {
     /// <summary> SSL certificate email. </summary>
-    public partial class AppServiceCertificateEmail : ResourceData
+    public partial class AppServiceCertificateEmail
     {
         /// <summary> Initializes a new instance of AppServiceCertificateEmail. </summary>
-        public AppServiceCertificateEmail()
+        internal AppServiceCertificateEmail()
         {
         }
 
         /// <summary> Initializes a new instance of AppServiceCertificateEmail. </summary>
-        /// <param name="id"> The id. </param>
-        /// <param name="name"> The name. </param>
-        /// <param name="resourceType"> The resourceType. </param>
-        /// <param name="systemData"> The systemData. </param>
         /// <param name="emailId"> Email id. </param>
         /// <param name="timeStamp"> Time stamp. </param>
-        /// <param name="kind"> Kind of resource. </param>
-        internal AppServiceCertificateEmail(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, string emailId, DateTimeOffset? timeStamp, string kind) : base(id, name, resourceType, systemData)
+        internal AppServiceCertificateEmail(string emailId, DateTimeOffset? timeStamp)
         {
             EmailId = emailId;
             TimeStamp = timeStamp;
-            Kind = kind;
         }
 
         /// <summary> Email id. </summary>
-        public string EmailId { get; set; }
+        public string EmailId { get; }
         /// <summary> Time stamp. </summary>
-        public DateTimeOffset? TimeStamp { get; set; }
-        /// <summary> Kind of resource. </summary>
-        public string Kind { get; set; }
+        public DateTimeOffset? TimeStamp { get; }
     }
 }
